97708608c216e78d51be4c137b8309905c7c739e
kelvinmburu <mburuhkelvin@gmail.com>
2022-06-06 00:40:49 +0300

    feat: added functions for all route pages

--- picsapp/views.py
+++ picsapp/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
     context = {}
     return render(request, 'picsapp/index.html',context)
+
+def login(request):
+    context = {}
+    return render(request, 'picsapp/login.html')
+
+def register(request):
+    context = {}
+    return render(request, 'picsapp/register.html')
+
+def main(request):
+    context = {}
+    return render(request, 'picsapp/main.html')
