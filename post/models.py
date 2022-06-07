3d182ef994abbad1f1935ff7ceff1a8b402ce10e
kelvinmburu <mburuhkelvin@gmail.com>
2022-06-07 20:07:21 +0300

    build: added user, post and follow models

--- post/models.py
+++ post/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
+from django.db.models.signals import post_save
+from django.utils.text import slugify
+from django.urls import reverse
 
 # Create your models here.
+
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=75, verbose_name='Tag')
+    slug = models.SlugField(null=False, unique=True)
+    
+    class Meta:
+        verbose_name_plural='Tags'
+        
+    def get_absolute_url(self):
+        return reverse('tags', args=(self.slug))
+    
+    def __str__(self):
+        self.title
+        
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+    
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    picture = models.ImageField(upload_to=user_directory_path, verbose_name='Picture', null=False)
+    caption = models.TextField(max_length=1500, verbose_name='Caption')
+    posted = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='tags')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField()
+    
+    
+    def get_absolute_url(self):
+        return reverse('postdetails', args=[str(self.id)])
+    
+    def __str__(self):
+        return self.posted
+    
+class Follow(models.Model):
+    followers = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    
+
