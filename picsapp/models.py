c7076ab94725382043c21b679c80c4b60d5254d8
kelvinmburu <mburuhkelvin@gmail.com>
2022-06-07 19:43:36 +0300

    build: added app post

--- picsapp/models.py
+++ picsapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-from django.utils.text import slugify
 
 # Create your models here.
 class Profile(models.Model):
