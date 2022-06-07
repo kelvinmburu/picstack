7007bf4e28aa06c32bde92a8900e57d5e723cd9b
kelvinmburu <mburuhkelvin@gmail.com>
2022-06-07 19:39:39 +0300

    feat: imported slugify

--- picsapp/models.py
+++ picsapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.utils.text import slugify
 
 # Create your models here.
 class Profile(models.Model):
