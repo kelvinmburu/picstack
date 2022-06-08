bccdd5d81543d19291552ec5fc3cef9a64e2b468
kelvinmburu <mburuhkelvin@gmail.com>
2022-06-08 09:23:44 +0300

    feat: added path for profile

--- picsapp/urls.py
+++ picsapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from picsapp.views import Signup,EditProfile
-from django.contrib.auth import views as authViews 
+from picsapp.views import Signup, EditProfile
+from django.contrib.auth import views as authViews
+from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('login/', authViews.LoginView.as_view(template_name='picsapp/login.html'), name='login'),
     path('signup/', Signup, name='signup'),
-    path('profile/edit', EditProfile, name='edit-profile'),
+    path('profile/edit', EditProfile, name='edit-profile')
 ]
